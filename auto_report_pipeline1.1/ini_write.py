ba3f97bcf468039c2e49ff0414a45f4303853375
Jc Wang <JWang294@slb.com>
2021-07-18 10:34:59 +0800

    add contact plots

--- auto_report_pipeline1.1/ini_write.py
+++ auto_report_pipeline1.1/ini_write.py
@@ -63,10 +63,13 @@
 
 
 
-import os, sys, re, logging, time, argparse
+import os, sys, re, logging, time, argparse, pptx, pptx.util, glob, re, csv, cv2, io
 import configparser
 import win32com.client as win32
 import xlwings as xw
+from pptx import Presentation
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
@@ -180,15 +183,198 @@ def call_Multi_macro():
     xl.Application.Run('Multi_Bit_Compare_v2.52.xlsm!Module1.Auto_Dynamic')
     logging.debug('Auto_Dynamic')
     xl.Application.ScreenUpdating = True
-        
+
+def read_doc_rop(org_dir1):
+    i = 0
+    doc_list = []
+    rop_list = []
+    for pic_dir in glob.glob(org_dir1 + r'\Contact\contact_script_test\summary_doc'):
+        # rop_data = pd.read_csv(pic_dir, sep = '\t',encoding = 'utf-8', errors="ignore")
+        # rop_data.head()
+        with open(pic_dir, "r", encoding="gbk", errors="ignore") as rop_data:
+            csv_reader = csv.reader(rop_data)
+            for row in csv_reader:
+                #save rop as a list
+                if i>=3:
+                    # rop_list[i-3] = list(str(row).split())[3]
+                    doc_list = str(row).split()
+                    print(isinstance(doc_list ,list))
+                    rop_list.append(doc_list[4])
+                    # print(rop_list[i-3])
+                    print(rop_list)
+                i += 1
+        break
+    return rop_list
+    
+def contact_pic_hooks(org_import_dir1, org_import_dir2, config_file_path):
+    cf = configparser.ConfigParser()
+    cf.read(config_file_path, encoding='utf-8')
+    template_path = cf.get("merge_template", "dirct")
+    prs = Presentation(template_path)
+    org_import_dir = (org_import_dir1, org_import_dir2)
+    contact_pic_path = ['','']
+    contact_pic = ['','']
+    i = 0
+    while i <= 1:
+        # org_path = os.walk(org_import_dir[i+1])
+        # for path, dir_list, file_list in org_path:
+            # for dir_name in dir_list:
+                # if re.search("\Contact\contact_script_test\utility_files", dir_name) != None:
+                    # logging.debug('invalid contact path:' + org_path + ", lacks essential utility_files")
+        for pic_dir in glob.glob(org_import_dir[i] + r'\Contact\contact_script_test\utility_files*'):
+                    # cf.set("Contact_pic_path", "pic_path"+ str(i), os.path.join(path, dir_name))
+            contact_pic_path[i] = pic_dir
+            break
+        i += 1
+    pic_left = [int(prs.slide_width*0.08), int(prs.slide_width*0.58)]
+    pic_top = int(prs.slide_height*0.3)
+    pic_width = int(prs.slide_width*0.4)
+    pic_height = int(prs.slide_height*0.6)
+    ipath = 0
+    time_title1 = cf.get("profile_title", "title1")
+    time_title2 = cf.get("profile_title", "title2")
+    #get rop number
+    rop_num_list = read_doc_rop(org_import_dir1)
+    # text_array = ("ROP Controlled: " + "50" + " ft/hr", "Build"+ time_title1, "Build" + time_title2)
+    # while ipath <= 1:
+    for contact_pic_path1 in glob.glob(contact_pic_path[ipath]+'/*.jpg'):
+        if re.search('Blade', contact_pic_path1) == None:
+            pic_type = contact_pic_path1.split('\\')[-1]
+            #get number of pic to match rop
+            bits_num = list(re.findall(r"\d+" ,str(pic_type.split('_')[0])))[-1]
+            text_array = ("ROP Controlled: " + str(rop_num_list[int(bits_num) - 1]) + " ft/hr", "Build"+ time_title1, "Build" + time_title2)
+            for contact_pic_path2 in glob.glob(contact_pic_path[ipath+1]+'/' + pic_type + '*'):
+                # contact_pic[1] = contact_pic_path2
+                slide = prs.slides.add_slide(prs.slide_layouts[1])
+                # slide.shapes.title.textframe.textrange.text = "test contact slide"
+                slide.shapes.title.text = "Contact Analysis Comparison - Blade Top Contact"
+                # pic2 = slide.shapes.add_picture()
+                img1 = cv2.imread(contact_pic_path[0])
+                img2 = cv2.imread(contact_pic_path[1])
+                pic1 = slide.shapes.add_picture(contact_pic_path1, pic_left[0], pic_top, pic_width, pic_height)
+                pic2 = slide.shapes.add_picture(contact_pic_path2, pic_left[1], pic_top, pic_width, pic_height)
+                print (contact_pic_path1)
+                i = 0
+                while i <= 2:
+                    if i == 0:
+                        tb = slide.shapes.add_textbox(300000, 700000, 1, 1)
+                    elif i == 1:
+                        tb = slide.shapes.add_textbox(1000000, 1600000, 1, 1)
+                    else:
+                        tb = slide.shapes.add_textbox(8000000, 1600000, 1, 1)
+                    p1 =  tb.text_frame.add_paragraph()
+                    p1.text = text_array[i]
+                    p1.font.size = pptx.util.Pt(25)
+                    i+=1
+
+    prs.save(template_path)
+
+def read_summary_doc(org_dir, list_type):
+    i = 0
+    iCase = 0
+    # doc_list = []
+    axis_num_list = []
+    # Connect zero point
+    axis_num_list.append(0)
+    for pic_dir in glob.glob(org_dir + r'\Contact\contact_script_test\summary_doc'):
+        with open(pic_dir, "r", encoding="gbk", errors="ignore") as rop_data:
+            csv_reader = csv.reader(rop_data)
+            for row in csv_reader:
+                doc_list = str(row).split()
+                #save rop/wob... as a list
+                if i >= 2 and i < 3:
+                    # doc_list = str(row).split()
+                    for type in doc_list:
+                        #match type to choose which line to return
+                        if (type.find(list_type)) != -1:
+                            iCase = doc_list.index(type)
+                            break
+                #add right num_line to axis_num_list
+                elif i > 3:
+                    axis_num_list.append(float(doc_list[iCase]))
+                i += 1
+        break
+    return axis_num_list
+
+def generate_contact_plots(org_dir1, org_dir2, config_path):
+    cf = configparser.ConfigParser()
+    cf.read(config_path, encoding='utf-8')
+    template_path = cf.get("merge_template", "dirct")
+    prs = Presentation(template_path)
+    org_import_dir = (org_dir1, org_dir2)
+    time_title1 = cf.get("profile_title", "title1")
+    time_title2 = cf.get("profile_title", "title2")
+    doc_type_list = ("doc", "tq", "rop")
+    pic_left = [int(prs.slide_width*0.08), int(prs.slide_width*0.58)]
+    pic_top = int(prs.slide_height*0.3)
+    pic_width = int(prs.slide_width*0.4)
+    pic_height = int(prs.slide_height*0.6)
+    for type in doc_type_list:
+        # plt.xticks(rotation=90)
+        # plt.gcf().autofmt_xdate()
+        # my_x_ticks = np.arange(10000,60000,10000)
+        # plt.xticks(my_x_ticks)
+        # line 1 points
+        x1 = read_summary_doc(org_dir1, "wob")
+        y1 = read_summary_doc(org_dir1, type)
+        # xpoints = np.array(x1)
+        # ypoints = np.array(y1)
+        # plotting the line 1 points 
+        plt.plot(x1, y1, label = "build" + time_title1, color='c')
+        # line 2 points
+        x2 = read_summary_doc(org_dir2, "wob")
+        y2 = read_summary_doc(org_dir2, type)
+        # plotting the line 2 points 
+        plt.plot(x2, y2, label = "build" + time_title2, color='m')
+        # add Horizontal line
+        plt.grid(axis="y")
+        # naming the x&y axis
+        if type == "doc":
+            y_axis_label = "Depth of Cut (in/rev)"
+            plot_title = "Depth of Cut-Wellington Shale @ 3ksi"
+            # add Horizontal line
+            plt.axhline(y2[3], color='orange', lw=2, alpha=0.7)
+            # fill with color
+            plt.axhspan(0, y2[3], alpha=0.5, color='y')
+            # plt.fill_between(x = , y = y2[3], y = 0, step="pre", color = "g", alpha = 0.3)
+            plt.ylim(0,0.6)
+        elif type == "tq":
+            y_axis_label = "Torque (ft-lbf)"
+            plot_title = "Torque-Wellington Shale @ 3ksi" 
+            plt.axvline(x2[4], color='gold', lw=2, alpha=0.7)
+            plt.ylim(0,10000)
+            # plt.hlines(0, xmin=0, xmax=10000, color='r', linestyle='-')
+            # plt.axis([10000,60000, 2000,10000])
+        else:
+            y_axis_label = "ROP (ft/hr)"
+            plot_title = "ROP-Wellington Shale @ 3ksi" 
+            plt.axvline(x2[4], color='orange', lw=2, alpha=0.7)
+            plt.ylim(0,450)
+            # plt.axis([10000,60000, 50,450])
+        plt.xlim(0,)
+        plt.xlabel('WOB(lbf)')
+        plt.ylabel(y_axis_label)
+        # giving a title to my graph
+        plt.title(plot_title)
+        plt.legend()
+        buffer_png = io.BytesIO()
+        plt.savefig(buffer_png, format = "png")
+        plt.show()
+        # # function to show the plot
+        slide = prs.slides.add_slide(prs.slide_layouts[1])
+        pic1 = slide.shapes.add_picture(buffer_png, pic_left[0], pic_top, pic_width, pic_height)
+        plt.close()
+        buffer_png.close()
+    prs.save(template_path)
+
 if __name__ == "__main__":
     cf = configparser.ConfigParser()
     cf.read(os.getcwd() + r'\auto_test_config.ini', encoding='utf-8')
     logging.debug('config file path:' + os.getcwd() + r'\auto_test_config.ini')
     # org_result_path1, org_result_path2, template_path = input("enter cases-results path1 & cases-result path2 & ppt_template-path separated with ',':").split(',')
     parser = argparse.ArgumentParser(description='input path argparse')
-    parser.add_argument('--path1', '-f', help='path1 property, non-essential parameters, have default values', default= "U:\result-backup\test-2021\test-20210416173550-lbo-0416cpc")
-    parser.add_argument('--path2', '-s', help='path2 property, non-essential parameters, have default values', default= "U:\result-backup\test-2021\test-20210702174422-lbo-0630cpc")
+    parser.add_argument('--path1', '-f', help='path1 property, non-essential parameters, have default values', default= r"U:\result-backup\test-2021\test-20210416173550-lbo-0416cpc")
+    parser.add_argument('--path2', '-s', help='path2 property, non-essential parameters, have default values', default= r"U:\result-backup\test-2021\test-20210702174422-lbo-0630cpc")
     parser.add_argument('--template_path', '-t', help='template_path property, non-essential parameters, have default values', default="")
     args = parser.parse_args()
     org_result_path1 = args.path1
@@ -213,12 +399,13 @@ if __name__ == "__main__":
     ini_write(config_dir[0], config_dir[1], os.getcwd() + r'\auto_test_config.ini')
     
     call_SAM_macro()
+    generate_contact_plots(org_result_path1, org_result_path2, template_path)
+    contact_pic_hooks(org_result_path1, org_result_path2, template_path)
     call_Multi_macro()
     
     # run_command = "start " + os.getcwd() + r'\test_SAM_v11.78.xlsm'
     # os.system(run_command)
     #ini_write(r'\\bgc-nas002\ideas_engine_folder\Share\Drilling_software_development\Work\Work-2021\release_testing_for_cpc\testing-cases', r'C:\Users\JWang294\Documents\projectfile\auto_test_config.ini')
     #config_sequence(r'\\bgc-nas002\ideas_engine_folder\Share\Drilling_software_development\Work\Work-2021\release_testing_for_cpc\testing-cases', r'\\bgc-nas002\ideas_engine_folder\Share\Drilling_software_development\Work\Work-2021\release_testing_for_cpc\testing-cases')
-    
     # C:\Users\JWang294\Documents\projectfile\test_config_cases\test-20210416173550-lbo-0416cpc
     # C:\Users\JWang294\Documents\projectfile\test_config_cases\test-20210702174422-lbo-0630cpc
