0f7054223a2c06476288369781a35e79876f5c80
Jc Wang <JWang294@slb.com>
2021-07-18 10:20:58 +0800

    add other line in plot

--- auto_report_pipeline1.1/test_generate_plots.py
+++ auto_report_pipeline1.1/test_generate_plots.py
@@ -11,6 +11,8 @@ def read_summary_doc(org_dir, list_type):
     iCase = 0
     # doc_list = []
     axis_num_list = []
+    # Connect zero point
+    axis_num_list.append(0)
     for pic_dir in glob.glob(org_dir + r'\Contact\contact_script_test\summary_doc'):
         with open(pic_dir, "r", encoding="gbk", errors="ignore") as rop_data:
             csv_reader = csv.reader(rop_data)
@@ -26,7 +28,7 @@ def read_summary_doc(org_dir, list_type):
                             break
                 #add right num_line to axis_num_list
                 elif i > 3:
-                    axis_num_list.append(doc_list[iCase])
+                    axis_num_list.append(float(doc_list[iCase]))
                 i += 1
         break
     return axis_num_list
@@ -52,38 +54,41 @@ def generate_contact_plots(org_dir1, org_dir2, config_path):
         # line 1 points
         x1 = read_summary_doc(org_dir1, "wob")
         y1 = read_summary_doc(org_dir1, type)
-        xpoints = np.array(x1)
-        ypoints = np.array(y1)
+        # xpoints = np.array(x1)
+        # ypoints = np.array(y1)
         # plotting the line 1 points 
-        plt.plot(xpoints, ypoints, label = "build" + time_title1
-         )
+        plt.plot(x1, y1, label = "build" + time_title1, color='c')
         # line 2 points
-        # x2 = read_summary_doc(org_dir2, "wob")
-        # y2 = read_summary_doc(org_dir2, type)
-        # # plotting the line 2 points 
-        # plt.plot(x2, y2, label = "build" + time_title2)
-        # plt.xticks(rotation=90)
-        # plt.gcf().autofmt_xdate()
-        # my_x_ticks = np.arange(10000,60000,10000)
-        # my_y_ticks = np.arange(-2, 2, 0.3)
-        # plt.xticks(my_x_ticks)
-        # plt.yticks(my_y_ticks)
+        x2 = read_summary_doc(org_dir2, "wob")
+        y2 = read_summary_doc(org_dir2, type)
+        # plotting the line 2 points 
+        plt.plot(x2, y2, label = "build" + time_title2, color='m')
+        # add Horizontal line
+        plt.grid(axis="y")
         # naming the x&y axis
         if type == "doc":
             y_axis_label = "Depth of Cut (in/rev)"
-            plot_title = "Depth of Cut-Wellington Shale @ 3ksi" 
-            # plt.ylim(0,0.6)
+            plot_title = "Depth of Cut-Wellington Shale @ 3ksi"
+            # add Horizontal line
+            plt.axhline(y2[3], color='orange', lw=2, alpha=0.7)
+            # fill with color
+            plt.axhspan(0, y2[3], alpha=0.5, color='y')
+            # plt.fill_between(x = , y = y2[3], y = 0, step="pre", color = "g", alpha = 0.3)
+            plt.ylim(0,0.6)
         elif type == "tq":
             y_axis_label = "Torque (ft-lbf)"
             plot_title = "Torque-Wellington Shale @ 3ksi" 
-            # plt.ylim(0,10000)
+            plt.axvline(x2[4], color='gold', lw=2, alpha=0.7)
+            plt.ylim(0,10000)
+            # plt.hlines(0, xmin=0, xmax=10000, color='r', linestyle='-')
             # plt.axis([10000,60000, 2000,10000])
         else:
             y_axis_label = "ROP (ft/hr)"
             plot_title = "ROP-Wellington Shale @ 3ksi" 
-            # plt.ylim(0,450)
+            plt.axvline(x2[4], color='orange', lw=2, alpha=0.7)
+            plt.ylim(0,450)
             # plt.axis([10000,60000, 50,450])
-        # plt.xlim(0,50000)
+        plt.xlim(0,)
         plt.xlabel('WOB(lbf)')
         plt.ylabel(y_axis_label)
         # giving a title to my graph
@@ -92,9 +97,6 @@ def generate_contact_plots(org_dir1, org_dir2, config_path):
         buffer_png = io.BytesIO()
         plt.savefig(buffer_png, format = "png")
         plt.show()
-        # buffer_png.seek(0)
-        # image = PIL.Image.open(buffer_png)
-        # plt.savefig('./test2.jpg')
         # # function to show the plot
         slide = prs.slides.add_slide(prs.slide_layouts[1])
         pic1 = slide.shapes.add_picture(buffer_png, pic_left[0], pic_top, pic_width, pic_height)
